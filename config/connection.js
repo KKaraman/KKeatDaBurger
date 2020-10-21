51b0e39ac1f9fbe4c0c023892202dfe4791578e1
Kerem Karaman <keremukaraman@gmail.com>
2020-10-20 20:38:41 -0500

    syncing to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ const mysql = require("mysql");
 require('dotenv').config()
 const sqlPwd = process.env.MYSQL
 
-const connection = mysql.createConnection({
+const connection;
+
+if(process.env.JAWSDB_URL){
+    connection= mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection= mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: sqlPwd,
   database: "delicious_burger_db"
 });
+}
 
 // Make connection.
 connection.connect((err) => {
